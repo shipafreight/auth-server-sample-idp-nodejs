f5d0eac64bcfc563a2b1dd5141a7923924217cf7
Hamza Purra <hapurra@agility.com>
2020-10-22 14:44:05 +0400

    extract keys to env vars; add page for decoding id tokens

--- index.js
+++ index.js
@@ -1,18 +1,19 @@
+const dotenv = require('dotenv')
 const assert = require('assert')
 const express = require('express')
 const jose = require('jose');
 const path = require('path');
 
+
+if (process.env.NODE_ENV === 'development') {
+  dotenv.config()
+}
+
 const app = express()
+
 app.use(express.urlencoded())
 app.set('view engine', 'ejs')
 app.set('views', path.resolve(__dirname, 'views'))
-
-// sign with partner's private key (SF Auth Server will then use partner's public key)
-const SIGNING_KEY = '-----BEGIN PRIVATE KEY-----\nMIIEtwIBADANBgkqhkiG9w0BAQEFAASCBKEwggSdAgEAAoIBAACX9GOASGo5dt5W\nKzeoLmG1aSkfnC5Qq51TeYiHwNCiyCVg3yW7dU3Ixc+mUp1gZwxOpG5f8ucE1quo\nSfBvfZwPW2i4MShft3JCFIIODaVi4sGnhoiWK/X3jyQm0sHYPqRGvecdHcjZJnzc\n9cNUjGWKv+/D+GBgael3EutM4raGocv9Be8r1W0fKIb3U+GM+0M2WXNapsGrlwmJ\nYKZBTCqhNS8sVgRyGG47KlIaKrhkmuelB+LtGnOe41iPk67m1kiK43Ib9wMBMeu1\nW6FNWGTwH5LARrTO8yQLfnegwHcojrMsOi50rWg0aMgcc+mGtTH+6pI/9fosmjl8\nh+4dHgcCAwEAAQKB/0DHk/+lX3ud/E0Z4YFWPJtc+IYcS/ZiIxDqF9Vf0tICp0Sz\n/N78tCtqgQqz35mwdKO2H8PRxsDeT5HP66tsDjLR0kPr2P1tNE2NdqBNGvucFdi4\nVtRGa3adVFkksjThB6GM5dEY2H4SlVqRfzM1IzjSeIHACj4wCLKVozBE2nFiUdfN\nzGvq73+Iq++WfuyNOjUZkEyHBbz5HFhv/AgsmcoHfPqSPXtVyS/zEkLU0CGaRP7W\nnSYLt8qeUnFSr/xmN0n015euLhjaLw4H7DhnEG9V2UxfEhVllRe3bqKXMTHsCEjO\nqnxL32wJckr6GDFcAfhmUtKckGwjIGxDd0xmuQKBgA0vXWdNfCI9ZZtl8IOVuSRR\nYq+/zZB+zg6MsMOK/AMOdRwrsypDAKOx2Ba369c4fjdO9O5ZX1TU01uyE8/K07qr\naa87LT7X0xVfy3GZiujGPwFsUSb8zBfWrEWl254cPsj4LcLqrxERHYER3tHCbNm2\nYYA9uqcyf5MpRdyXu5BFAoGAC4ZY+kRR3pGNSxaY0biNlj+InxsB9wXQREuJNwA9\nJwGhhp5/npoXv+JxUC9Vj4jk355ultRvexwX8AX/+8WplkUn6PI3OhTG2A4pRnjA\nnoHtFfiZgfoIvgNs3qdwCBGnx5kHZmu43wCViVdKr9cmuFl/fKUERfu95CzjvNVQ\nl9sCgYAFUWJJ5R25LaxKpi4A1KWs+UplemXt7scrZSFybR9CGFWxvApydD3QyE1Q\ntHPiBX4azrTJYbCqpYBuDoAhIjyNoM9qhuKz4xbXGyESO4ykYDlPWKHOqM2km8g1\n+zR+LARCyJ6kbEWaC54rzcpiLhn7Ia1nrP4Yq6x67X2pAGX9RQKBgAPgG4+c5cLo\nQxEa+iyb2NDLcI/ALzr3837jv7KFHAs/2yc/qT6WJqJZ7Sy/ng/0QrKHvjxuF0bY\nvZvzCI+LemmTu8XznWqkjfEdYffWpz8mBam7vTMd906eLD/7Igs0lKzj4r8oPjSP\nJ7nVZsIWoWigf+RUQUmfS82s4rAfM0i9AoGABmwUJJPG5DRErwmknNeaLGgsSIC2\nUqgqfueSjS4yKNjUP+bg27NANKxEyBBaB/MJPZQl5cR/SxcbOKQM4cdQqn+eXZD8\ndltW/GcP3K+nrM0G5OUGfr1p1V9/d4iDJz/l9buFwmTnmWwb7HjXc7Ie3qnz/puZ\nQVDwcNwRAxox0T4=\n-----END PRIVATE KEY-----'
-
-// encrypt using SF Auth Server's public key
-const ENCRYPTION_KEY = '-----BEGIN PUBLIC KEY-----\nMIIBITANBgkqhkiG9w0BAQEFAAOCAQ4AMIIBCQKCAQAAmSW8tmE1zJwyVbi2imOe\nVfTnNuyyA7QxX94Iu0S2kyK9CbqlMDWoObk8ZjWh7WZP8v0o3T/ZspMF8nyuVvHF\nudhP9yAtaN5etvxFd3imYVt+tt2lA/vBToT+Yg5Vd5W5yOqbSTgb/sWl/2piU1Pa\nwlWX/aJbyS/MiNRkFUA/k+v1Y9BQmNVgxQJyDwVuFKDiMiy/yhdpgrA/JcLvXtIy\nG8OZ0aAhEo8sF/OrqfVKtGdbmrOpsz0j7YQfyZKPjw2AnyuPRk3gMPhzmPWPM4zS\nAMHRIJxSJnHYU+63jtFd2zFLp4+shZGDywYbeDhjpj5kw4TDcV/QRmwiZ2dvLbhT\nAgMBAAE=\n-----END PUBLIC KEY-----'
 
 const CLIENT_ID = '4788d283-835d-4b57-8599-20d886d3d806'
 const VALID_REDIRECT_URIS = [
@@ -22,8 +23,8 @@ const VALID_REDIRECT_URIS = [
 ]
 
 const signAndEncrypt = (payload) => {
-  const signed = jose.JWS.sign(payload, SIGNING_KEY) // verify it's from partner
-  const encrypted = jose.JWE.encrypt(signed, ENCRYPTION_KEY) // only SF can see the ID token (private key)
+  const signed = jose.JWS.sign(payload, process.env.SIGN_PRIVATE_KEY) // verify it's from partner
+  const encrypted = jose.JWE.encrypt(signed, process.env.ENC_PUBLIC_KEY) // only SF can see the ID token (private key)
   return encrypted
 }
 
@@ -79,6 +80,42 @@ app.get('/auth/register', (req, res, next) => {
     clientId: req.query.client_id,
     responseType: req.query.response_type,
     redirectUri: encodeURIComponent(req.query.redirect_uri),
+  })
+})
+
+/**
+ * render page to verifying token
+ */
+app.get('/auth/decode-token', (req, res, next) => {
+  return res.render('decode-token', {
+    title: 'Decode ID Token',
+    payload: null,
+    encryptedToken: null,
+    signingPublicKey: null,
+  })
+})
+
+/**
+ * render page to verifying token
+ */
+app.post('/auth/decode-token', (req, res, next) => {
+  const encryptedToken = req.body.id_token
+  const signingPublicKey = req.body.sign_public_key
+
+  let payload
+  
+  try {
+    const decryptedToken = jose.JWE.decrypt(encryptedToken, process.env.ENC_PRIVATE_KEY).toString()
+    payload = jose.JWS.verify(decryptedToken, signingPublicKey)
+  } catch (err) {
+    payload = 'Failed to decode the token'
+  }
+
+  return res.render('decode-token', {
+    title: 'Decode ID Token',
+    payload,
+    encryptedToken,
+    signingPublicKey,
   })
 })
 
